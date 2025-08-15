2b6b31da458cfddcbcda9fd20e352c39517c6280
ali usman <mrau429040@gmail.com>
2025-08-16 00:27:48 +0500

    Add TypeScript interfaces for button components

---
+++ client/src/interfaces/ui/ButtonProps.ts
@@ -0,0 +1,31 @@
+import { TouchableOpacityProps } from 'react-native';
+
+export interface ButtonProps extends TouchableOpacityProps {
+	label: string;
+	loading?: boolean;
+	disabled?: boolean;
+
+	Icon?: React.ComponentType<{ size?: number; color?: string; name?: string }>;
+	iconPosition?: 'start' | 'end'; // default: 'start'
+
+	bgVariant?:
+		| 'primary'
+		| 'secondary'
+		| 'danger'
+		| 'outline'
+		| 'success'
+		| 'gradient'; // default: 'gradient'
+	textVariant?:
+		| 'default'
+		| 'primary'
+		| 'secondary'
+		| 'danger'
+		| 'success'
+		| 'gradient'; // default: 'gradient'
+
+	size?: 'sm' | 'md' | 'lg'; // default: 'md'
+	accessibilityLabel?: string;
+	accessibilityRole?: 'button' | 'link';
+
+	className?: string;
+}
