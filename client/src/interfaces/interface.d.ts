5f961825bc5277af0edab3898c5e7cb25fb72bca
ali usman <mrau429040@gmail.com>
2025-08-16 09:36:41 +0500

    Add interface for onboarding item

--- client/src/interfaces/interface.d.ts
+++ client/src/interfaces/interface.d.ts
@@ -1,2 +1,3 @@
-export * from "./ui/ButtonProps";
+export * from './ui/ButtonProps';
+export * from './ui/onboardingType';
 // import { InputFieldProps, LoginFormData } from '@/interface/interface';
